b4e09658b1bf850ea9344d51f02cbf96443c786e
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2019-12-15 16:51:18 +0100

    Added the IGameTile interface

---
+++ SlidingTiles/Interfaces/IGameTile.cs
@@ -0,0 +1,18 @@
+﻿using UIKit;
+
+namespace SlidingTiles.Interfaces
+{
+    public interface IGameTile
+    {
+        /// <summary>
+        /// Draw a text onto a specified image
+        /// </summary>
+        /// <param name="uiImage">The image to use</param>
+        /// <param name="sText">Text to place in the image</param>
+        /// <param name="textColor">Color of the text</param>
+        /// <param name="FontSize">Font size of the text</param>
+        /// <returns>An image with a text</returns>
+        UIImage DrawTileText(
+            UIImage uiImage, string sText, UIColor textColor, int FontSize);
+    }
+}
